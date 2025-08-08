103b4b5e62436fe25153afac7b1e5c587d0e78b3
AshokSamrat292 <cse24019@iiitkalyani.ac.in>
2025-08-08 12:52:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/api/sensors', async (req, res) => {
     return res.status(400).json({ error: "Missing one or more sensor fields" });
   }
 
-  const fireDetected = Number(temp) > 30 && Number(smoke) == 1 && Number(ldr) > 25;
+  const fireDetected = Number(temp) > 20 && Number(smoke) == 0 && Number(ldr) > 15;
 
   latestData = {
     ldr,
